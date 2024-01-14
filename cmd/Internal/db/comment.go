29499387d2c6e4b85683287464a80fd92fe296b8
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2024-01-15 05:09:20 +0530

    Implementing Database Package with Update and Delete Method

--- cmd/Internal/db/comment.go
+++ cmd/Internal/db/comment.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 
 	"github.com/kanishkanaik/go-rest-api-course/cmd/Internal/comment"
+	uuid "github.com/satori/go.uuid"
 )
 
 type CommentRow struct {
@@ -38,4 +39,63 @@ func (d *Database) GetComment(ctx context.Context, uuid string) (comment.Comment
 	}
 
 	return convertCommentRowToComment(cmtRow), nil
+}
+
+func (d *Database) PostComment(ctx context.Context, cmt comment.Comment) (comment.Comment, error) {
+	cmt.ID = uuid.NewV4().String()
+	postRow := CommentRow{
+		ID:     cmt.ID,
+		Slug:   sql.NullString{String: cmt.Slug, Valid: true},
+		Body:   sql.NullString{String: cmt.Body, Valid: true},
+		Author: sql.NullString{String: cmt.Author, Valid: true},
+	}
+	rows, err := d.Client.NamedQueryContext(ctx,
+		`INSERT INTO comments (id, slug, author, body) VALUES (:id, :slug, :author, :body)`, postRow)
+
+	if err != nil {
+		return comment.Comment{}, fmt.Errorf("Cound not Add Comment: %w", err)
+	}
+
+	if err := rows.Close(); err != nil {
+		return comment.Comment{}, fmt.Errorf("Could not Close Row: %w", err)
+	}
+
+	return cmt, nil
+
+}
+
+func (d *Database) DeleteComment(ctx context.Context, id string) error {
+	_, err := d.Client.ExecContext(ctx, `DELETE FROM comments where id $1`, id)
+	if err != nil {
+		return fmt.Errorf("Could not able to Delete Comment")
+	}
+
+	return nil
+}
+
+func (d *Database) UpdateComment(ctx context.Context, cmt comment.Comment, id string) (comment.Comment, error) {
+
+	cmtRow := CommentRow{
+		ID:     id,
+		Slug:   sql.NullString{String: cmt.Slug, Valid: true},
+		Body:   sql.NullString{String: cmt.Body, Valid: true},
+		Author: sql.NullString{String: cmt.Author, Valid: true},
+	}
+	rows, err := d.Client.NamedQueryContext(ctx,
+		`UPDATE comments SET 
+		slug = :slug, 
+		author= :author,
+		body: =body
+		WHERE id= :id`, cmtRow)
+
+	if err != nil {
+		return comment.Comment{}, fmt.Errorf("Cound not Update Comment: %w", err)
+	}
+
+	if err := rows.Close(); err != nil {
+		return comment.Comment{}, fmt.Errorf("Could not Close Row: %w", err)
+	}
+
+	return convertCommentRowToComment(cmtRow), nil
+
 }
